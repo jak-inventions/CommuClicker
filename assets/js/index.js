45df484b2676ce9a39a14a3fdfdc2e0a3978fbe6
Jak <jaksawesome10@github.com>
2019-07-12 18:35:51 -0400

    firebase stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,21 @@
 
+/*service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read, write: if request.auth != null;
+    }
+  }
+}
+
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if false;
+    }
+  }
+}*/
+
 function increment(){
 
 
