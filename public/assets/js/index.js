abcca53e8a3d2dec3b90f9eb0e4d21d85bd721bb
Jak <jaksawesome10@github.com>
2019-07-13 17:04:04 -0400

    disables double tap to zoom on mobile

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -36,3 +36,22 @@ function pxToInt(pixelForm){
   return parseInt(pixelForm.substring(0, pixelForm.indexOf("px")))
 
 }
+
+//Disables double tap to zoom
+
+(function($) {
+  $.fn.nodoubletapzoom = function() {
+      $(this).bind('touchstart', function preventZoom(e) {
+        var t2 = e.timeStamp
+          , t1 = $(this).data('lastTouch') || t2
+          , dt = t2 - t1
+          , fingers = e.originalEvent.touches.length;
+        $(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
+      });
+  };
+})(jQuery);
