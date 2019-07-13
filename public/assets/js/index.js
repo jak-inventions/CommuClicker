4facbbd983810f57b8b60d6ee5f95e3731990f5d
Jak <jaksawesome10@github.com>
2019-07-13 19:14:48 -0400

    stuff

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,6 +17,20 @@ function increment(){
 
 }
 
+function plusAnimation(){makePlus();}
+
+function makePlus(){
+
+  var ele = document.createElement('p');
+  ele.textContent = "+1";
+  ele.setAttribute('class', 'PlusOne');
+  document.body.appendChild(ele);
+  ele.style.position = "absolute";
+  ele.style.top = (225 - 500) + "px";
+  ele.style.left = (375 - 650) + "px";
+
+}
+
 function pxToInt(pixelForm){
 
   return parseInt(pixelForm.substring(0, pixelForm.indexOf("px")))
