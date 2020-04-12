0c8236823b1bb3a2de8cc0c728f4722270e761c6
Jak <jaksawesome10@gmail.com>
2020-04-12 15:13:46 -0400

    basic stuff

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
+
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Running on port ${port}`));
 
