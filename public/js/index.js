7f3d8e81bcfd3957683c9ae893556c6c7dae0fef
Jak <jaksawesome10@gmail.com>
2020-04-17 20:14:48 -0400

    text overflow thing

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,7 @@ window.onload = () => {
   xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
   xhttp.send();
   xhttp.onload = function() {
-    document.getElementById('scoreCount').innerText = numberWithCommas(xhttp.responseText);
+    document.getElementById('scoreCount').innerText = parseScore(xhttp.responseText);
   }
 }
 
@@ -16,7 +16,7 @@ document.getElementById('clicker').onclick = () => {
   xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
   xhttp.send();
   xhttp.onload = function() {
-    document.getElementById('scoreCount').innerText = numberWithCommas(xhttp.responseText);
+    document.getElementById('scoreCount').innerText = parseScore(xhttp.responseText);
   }
 }
 
@@ -26,6 +26,28 @@ function toggleSettings(){
   document.getElementById("settings").classList.toggle("open");
 }
 
+function parseScore(score){
+  return numberWithCommas(toFixed(score));
+}
+
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function toFixed(x) {
+  if (Math.abs(x) < 1.0) {
+    var e = parseInt(x.toString().split('e-')[1]);
+    if (e) {
+        x *= Math.pow(10,e-1);
+        x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+    }
+  } else {
+    var e = parseInt(x.toString().split('+')[1]);
+    if (e > 20) {
+        e -= 20;
+        x /= Math.pow(10,e);
+        x += (new Array(e+1)).join('0');
+    }
+  }
+  return x;
 }
