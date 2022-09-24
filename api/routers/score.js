ca4f1dbce0446329c9ffd518e8d5f375395b3ed7
John Khachian <jaksawesome10@gmail.com>
2022-09-24 16:31:27 -0400

    score command

--- api/routers/score.js
+++ api/routers/score.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// PSQL command to get world score
+// SELECT SUM (score) FROM users WHERE true;
+
 router.get("/", (req, res) => {
     res.send(`${0}`);
 });
